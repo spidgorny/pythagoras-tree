db15f46c1653d98911565ddb8d68f647c2593a94
DEPIDSVY <depidsvy@nintendo.de>
2019-12-19 16:02:49 +0100

    - add some pyth boxes

--- src/pythagoras.ts
+++ src/pythagoras.ts
@@ -1,15 +1,41 @@
 // import * as Two from 'twojs-ts';
-import * as Two from 'two.js';
+import {default as TwoCons} from 'two.js';
+import {Two, Vector} from "twojs-ts";
 
-console.dir(Two);
-
-const two = new Two.default({
+const two: Two = new TwoCons({
 	fullscreen: true,
 	autostart: true
 }).appendTo(document.body);
 
-var rect = two.makeRectangle(two.width / 2, two.height / 2, 250, 250);
-two.bind('update', function () {
-	rect.rotation += 0.001;
-});
+function rotatingBox() {
+	const rect = two.makeRectangle(two.width / 2, two.height / 2, 250, 250);
+	two.bind('update', function () {
+		rect.rotation += 0.001;
+	});
+}
 
+function pythagoras() {
+	let w = 100;
+	let center = new Vector(two.width / 2, two.height / 2);
+	renderBox(center, w, 0, 3);
+	two.update();
+}
+
+function renderBox(center: Vector, w: number, rotation: number, level?: number) {
+	if (!level) {
+		return;
+	}
+	const rect = two.makeRectangle(center.x, center.y, w, w);
+	rect.rotation = rotation;
+
+	w /= 2;
+
+	const centerLeft = new Vector(center.x, center.y).addSelf(new Vector(-w, -w));
+	renderBox(centerLeft, w, rotation + Math.PI / 4, level - 1);
+
+	const centerRight = new Vector(center.x, center.y).addSelf(new Vector(w, -w));
+	renderBox(centerRight, w, rotation - Math.PI / 4, level - 1);
+}
+
+// rotatingBox();
+pythagoras();
