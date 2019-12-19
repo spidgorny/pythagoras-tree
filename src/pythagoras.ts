0fd638bfe77f71f34deb2ff4ebe1d2663e30f25a
DEPIDSVY <depidsvy@nintendo.de>
2019-12-19 17:27:58 +0100

    - try different options

--- src/pythagoras.ts
+++ src/pythagoras.ts
@@ -1,40 +1,57 @@
 // import * as Two from 'twojs-ts';
 import {default as TwoCons} from 'two.js';
-import {Two, Vector} from "twojs-ts";
+import {Group, Two, Vector} from "twojs-ts";
 
 const two: Two = new TwoCons({
 	fullscreen: true,
-	autostart: true
+	autostart: true,
 }).appendTo(document.body);
 
 function rotatingBox() {
 	const rect = two.makeRectangle(two.width / 2, two.height / 2, 250, 250);
-	two.bind('update', function () {
+	two.bind('update', () => {
+		console.log('update');
 		rect.rotation += 0.001;
+		// two.render();
 	});
-}
-
-function pythagoras() {
-	let w = 100;
-	let center = new Vector(two.width / 2, two.height / 2);
-	renderBox(center, w, 0, 3);
 	two.update();
 }
 
-function renderBox(center: Vector, w: number, rotation: number, level?: number) {
-	if (!level) {
-		return;
-	}
+function pythagoras() {
+	let w = 200;
+	let center = new Vector(0, 0);
+	const figure = renderBox(center, w, 0, 5);
+	figure.translation = new Vector(two.width / 2, two.height * 0.8);
+	two.bind('update', () => {
+		figure.rotation += 0.001;
+	});
+	two.update();
+}
+
+function renderBox(center: Vector, w: number, rotation: number, level?: number): Group {
 	const rect = two.makeRectangle(center.x, center.y, w, w);
-	rect.rotation = rotation;
 
-	w /= 2;
+	if (!level) {
+		return rect;
+	}
 
-	const centerLeft = new Vector(center.x, center.y).addSelf(new Vector(-w, -w));
-	renderBox(centerLeft, w, rotation + Math.PI / 4, level - 1);
+	const w4 = w * 0.4;
+	const w6 = w * 0.6;
 
-	const centerRight = new Vector(center.x, center.y).addSelf(new Vector(w, -w));
-	renderBox(centerRight, w, rotation - Math.PI / 4, level - 1);
+	const group1 = renderBox(center, w4,  -Math.PI * w4 / 400, level - 1);
+	group1.translation = new Vector(-w/2.4, -w/1.3);
+	// group1.rotation = rotation + Math.PI / 9;
+	// group1.scale = 1 / level;
+
+	// const centerRight = new Vector(center.x, center.y).addSelf();
+	const group2 = renderBox(center, w6,  Math.PI * 0.06, level - 1);
+	group2.translation = new Vector(w/2.4, -w / 1.1);
+	// group2.rotation = -Math.PI / 100;
+	// group2.scale = 1 / level;
+
+	const group = two.makeGroup(rect, group1, group2);
+	group.rotation = rotation;
+	return group;
 }
 
 // rotatingBox();
